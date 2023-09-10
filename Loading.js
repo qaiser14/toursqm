41904a83e059739860320ad4d450f79acd707dcd
Qaiser Malik <85894795+qaiser14@users.noreply.github.com>
2023-09-10 15:17:25 +1000

    Add files via upload

---
+++ Loading.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const Loading = () => {
+
+  return (
+    <div className='loading'>
+      <h1>loading...</h1>
+    </div>
+  );
+};
+
+export default Loading;
+
+
+
+
+
+
+
+// import React from 'react';
+
+// const Loading = () => {
+//   return (
+//     <div className="loading">
+//       <h1>loading...</h1>
+//     </div>
+//   );
+// };
+
+// export default Loading;
